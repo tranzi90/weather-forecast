b0c5b31f8d0a1e9908eab0a8aac2b10fc95d4056
tranzi90 <tranzi90@gmail.com>
2018-11-01 17:28:32 +0300

    Setup start script & heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -51,6 +52,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Серв ебашит на 3000 порту епте!!!!!!!11');
+app.listen(port, () => {
+    console.log(`Серв ебашит на ${port} порту епте!!!!!!!11`);
 });
