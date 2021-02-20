0481aefcc252b456c9edc403aabf46b5df35495f
denis <tranzi90@gmail.com>
2021-02-20 22:23:01 +0300

    prettify client-side code

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
-const weatherForm = document.querySelector('form');
-const search = document.querySelector('input');
-const result = document.querySelector('#result');
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const result = document.querySelector('#result')
 
 weatherForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    result.textContent = 'Loading...';
+    result.textContent = 'Loading...'
 
     fetch(`/weather?address=${search.value}`).then(function (response) {
         response.json().then(function (data) {
             result.textContent = JSON.stringify(data)
         })
     })
-});
+})
