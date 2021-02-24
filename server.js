7c4a15341ca5d3204c4aea65b47bc9c60c4f1ffe
denis <tranzi90@gmail.com>
2021-02-24 23:11:58 +0300

    prettify code

--- server.js
+++ server.js
@@ -1,71 +1,71 @@
-const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
-const geocode = require('./utils/geocode');
-const weather = require('./utils/weather');
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+const geocode = require('./utils/geocode')
+const weather = require('./utils/weather')
 
-const port = process.env.PORT || 3000;
-var app = express();
+const port = process.env.PORT || 3000
+var app = express()
 
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
 
 app.use((req, res, next) => {
-    var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;
-    console.log(log);
+    var now = new Date().toString()
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
     fs.appendFile('server.log', log + '\n', (err) => {
-        if (err)
-            console.log('Не удалось записать логи в файл');
-    });
-    next();
-});
+        if (err) console.log('Не удалось записать логи в файл')
+    })
+    next()
+})
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear();
-});
+    return new Date().getFullYear()
+})
 
 hbs.registerHelper('screamIt', (text) => {
-    return text.toUpperCase();
-});
+    return text.toUpperCase()
+})
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeMessage: 'welcome!'
-    });
-});
+        welcomeMessage: 'welcome!',
+    })
+})
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page'
-    });
-});
+        pageTitle: 'About Page',
+    })
+})
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address)
-        return res.send({error: 'Address is required.'});
+    if (!req.query.address) return res.send({ error: 'Address is required.' })
 
     geocode(req.query.address, (errorMessage, results) => {
-        if (errorMessage)
-            res.send({error: errorMessage});
+        if (errorMessage) res.send({ error: errorMessage })
         else {
-            weather(results.Latitude, results.Longitude, (errorMessage, weatherResults) => {
-                if (errorMessage)
-                    res.send(errorMessage);
-                else {
-                    res.send({
-                        forecast: weatherResults,
-                        location: results.Address
-                    });
+            weather(
+                results.Latitude,
+                results.Longitude,
+                (errorMessage, weatherResults) => {
+                    if (errorMessage) res.send(errorMessage)
+                    else {
+                        res.send({
+                            forecast: weatherResults,
+                            location: results.Address,
+                        })
+                    }
                 }
-            });
+            )
         }
-    });
-});
+    })
+})
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    console.log(`Server is running on port ${port}`)
+})
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,27 +1,27 @@
-const request = require('request');
+const request = require('request')
 
 var geocodeAddress = (a, callback) => {
+    var address = encodeURIComponent(a)
 
-    var address = encodeURIComponent(a);
+    request(
+        {
+            url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyB89e73KrMzRb1iyt0dPaiMpKVHJxp17As`,
+            json: true,
+        },
+        (error, response, body) => {
+            if (error) {
+                callback('Unable to connect to Google servers.')
+            } else if (body.status === 'ZERO_RESULTS') {
+                callback('Unable to find that address.')
+            } else if (body.status === 'OK') {
+                callback(undefined, {
+                    Address: body.results[0].formatted_address,
+                    Latitude: body.results[0].geometry.location.lat,
+                    Longitude: body.results[0].geometry.location.lng,
+                })
+            }
+        }
+    )
+}
 
-    request({
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyB89e73KrMzRb1iyt0dPaiMpKVHJxp17As`,
-        json: true
-    }, (error, response, body) => {
-        if (error) {
-            callback('Unable to connect to Google servers.');
-        }
-        else if (body.status === "ZERO_RESULTS") {
-            callback('Unable to find that address.');
-        }
-        else if (body.status === "OK") {
-            callback(undefined, {
-                Address: body.results[0].formatted_address,
-                Latitude: body.results[0].geometry.location.lat,
-                Longitude: body.results[0].geometry.location.lng
-            })
-        }
-    });
-};
-
-module.exports = geocodeAddress;
+module.exports = geocodeAddress
